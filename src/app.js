5cbbe68ad46e2fd336c46116560e4f44ffb3a0d0
abarrelier <128666422+abarrelier@users.noreply.github.com>
2023-06-15 05:34:53 +0000

    Generador de Excusa JS con funciones

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function excuseGenerator(who, action, what, when) {
+  let numWho = Math.floor(Math.random() * who.length);
+  let numAction = Math.floor(Math.random() * action.length);
+  let numWhat = Math.floor(Math.random() * what.length);
+  let numWhen = Math.floor(Math.random() * when.length);
+  return (
+    who[numWho] +
+    " " +
+    action[numAction] +
+    " " +
+    what[numWhat] +
+    " " +
+    when[numWhen]
+  );
+}
+
+function onLoad() {
+  var excusa = document.getElementById("excuse");
+  excusa.innerHTML = excuseGenerator(who, action, what, when);
+}
+
+window.onload = onLoad;
