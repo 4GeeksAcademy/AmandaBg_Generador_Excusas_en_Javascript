0b4d80c0e800b8706105d6a50992936bfdb8cd6b
abarrelier <128666422+abarrelier@users.noreply.github.com>
2023-06-19 05:24:13 +0000

    Le agregue comentarios

--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
 /* eslint-disable */
+//Importar bibliotecas
+
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
+//Array, arreglo, es una estructura de datos que se utiliza para almacenar una colección de elementos del mismo tipo.
+//Es una forma de organizar y acceder a los datos de manera secuencial,
+//donde cada elemento tiene una posición o índice único dentro del array.
+let who = ["El perro", "Mi abuela", "Su tortuga", "Mi pájaro"];
+let action = ["se comió", "orinó", "aplastó", "rompió"];
+let what = ["mi tarea", "las llaves", "el coche"];
 let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
+  "antes de la clase",
+  "justo a tiempo",
+  "cuando terminé",
+  "durante mi almuerzo",
+  "mientras rezaba"
 ];
 
 function excuseGenerator(who, action, what, when) {
-  let numWho = Math.floor(Math.random() * who.length);
-  let numAction = Math.floor(Math.random() * action.length);
-  let numWhat = Math.floor(Math.random() * what.length);
-  let numWhen = Math.floor(Math.random() * when.length);
+  // Función para generar una excusa
+  let numWho = Math.floor(Math.random() * who.length); // Obtener un índice aleatorio para 'who'
+  let numAction = Math.floor(Math.random() * action.length); // Obtener un índice aleatorio para 'action'
+  let numWhat = Math.floor(Math.random() * what.length); // Obtener un índice aleatorio para 'what'
+  let numWhen = Math.floor(Math.random() * when.length); // Obtener un índice aleatorio para 'when'
   return (
     who[numWho] +
     " " +
@@ -29,12 +35,12 @@ function excuseGenerator(who, action, what, when) {
     what[numWhat] +
     " " +
     when[numWhen]
-  );
+  ); // Devolver una excusa generada combinando los elementos aleatorios de cada array
 }
 
 function onLoad() {
-  var excusa = document.getElementById("excuse");
-  excusa.innerHTML = excuseGenerator(who, action, what, when);
+  var excusa = document.getElementById("excuse"); // Obtener el elemento con el ID 'excuse'
+  excusa.innerHTML = excuseGenerator(who, action, what, when); // Generar una excusa y asignarla al contenido del elemento
 }
 
-window.onload = onLoad;
+window.onload = onLoad; // Asignar la función onLoad para que se ejecute cuando se cargue la ventana
